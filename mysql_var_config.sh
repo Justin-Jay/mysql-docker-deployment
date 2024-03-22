ba79173ce78581b578941fcfa8f07addbff7a268
Justin.Maboshego@gmail.com <justin.maboshego@gmail.com>
2024-03-22 11:36:35 +0200

    @Justin: spring batch schema

--- mysql_var_config.sh
+++ mysql_var_config.sh
@@ -6,6 +6,7 @@ export compose_dir=/home/justin/mysql-docker-deployment/mysql/
 
 
 export schema_init_file_name=init.sql
+export spring_batch_schema_init_file_name=spring_batch.sql
 export data_init_file_name=data.sql
 export mysql_var_file_name=mysql-vars.env
 export mysql_cnf_file_name=my.cnf
